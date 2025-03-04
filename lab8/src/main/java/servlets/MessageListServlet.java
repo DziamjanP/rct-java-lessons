26ea184456e4cc463a44d77a2f0fdd1a891010a6
DziamjanP <demyan.postnikov@gmail.com>
2025-03-04 19:08:00 +0300

    set chat window refresh rate to 1

--- lab8/src/main/java/servlets/MessageListServlet.java
+++ lab8/src/main/java/servlets/MessageListServlet.java
@@ -19,7 +19,7 @@ public class MessageListServlet extends ChatServlet {
         PrintWriter pw = response.getWriter();
         // Записть в поток HTML-разметку страницы
         pw.println(
-                "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
+                "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='1'></head>");
         pw.println("<body>");
         // В обратном порядке записать в поток HTML-разметку для каждого сообщения
         for (int i = messages.size() - 1; i >= 0; i--) {
